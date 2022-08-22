79e23b090edbf646d2704b7c3423c77f125ddc8e
duynguyen224 <42030245+duynguyen224@users.noreply.github.com>
2022-08-22 15:51:41 +0700

    first commit

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import React from "react";
+import Meta from "../components/Meta";
+
+function about() {
+    return (
+        <div>
+            <Meta title="About"/>
+            <h1>About page</h1>
+        </div>
+    );
+}
+
+export default about;
